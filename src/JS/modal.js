c89c4372a2a5d727f5f1e81af3a3477431cd383a
Hanna Rogalska <hanrogalska@gmail.com>
2023-04-17 18:33:50 +0200

    commit

--- src/JS/modal.js
+++ src/JS/modal.js
@@ -1,7 +1,9 @@
+
 const close = document.querySelector('.close_modal_window');
 const modal = document.querySelector('.modal_window');
-
 
 close.addEventListener('click', () => {
     modal.style.display = 'none'
 })
+
+
