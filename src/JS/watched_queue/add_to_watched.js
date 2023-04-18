5905feeb55455cdcab1f7e0129369de8493b9607
KacperMadajewski <kacpermadajewski@gmail.com>
2023-04-18 18:26:47 +0200

    Próba stworzenia funkcji dla przycisku "add to watched"

--- src/JS/watched_queue/add_to_watched.js
+++ src/JS/watched_queue/add_to_watched.js
@@ -4,4 +4,48 @@
 // - push array to localStorage and to watched / queue library
 // - separate movies by "id" ????
 // - get only matched elements and add them to same array and make specific new object of movies 
-// - I think i need to use ".map", "querrySelector", "addEventListener", "async/await"
+// - I think i need to use ".map", "querrySelector", "addEventListener", "async/await" etc....
+
+const API_KEY = '28f50cf3f177782503c21b43af04c7bc';
+
+const url1 = 'https://api.themoviedb.org/3/';
+
+const dataArray = [];
+
+//FETCH najpopularniejszych na dziś filmów
+const url =
+  'https://api.themoviedb.org/3/trending/all/day?api_key=28f50cf3f177782503c21b43af04c7bc';
+
+const getPopularMoviesData = async () => {
+  try {
+    const response = await fetch(url);
+      const data = await response.json();
+      const popularMovies = await data.results;
+      dataArray.push(popularMovies);
+      return dataArray;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+
+
+
+
+
+
+const watchedArray = [];
+
+const addToWatchedBtn = document.querySelector(/*Dodać klasę/id buttona z modala */);
+
+addToWatchedBtn.addEventListener("click", addToWatchedArray);
+
+// FUNKCJA NA DODAWANIE 'MOVIE' DO ODPOWIEDNIEGO 'ARRAY'
+
+const addToWatchedArray = () => {
+    const elementId = 1 /* WZIĄĆ 'ID' FILMU KTÓRY JEST WYŚWIETLANY W MODALU */; 
+    const objectMovie = dataArray.find(elementId);
+    watchedArray.push(objectMovie)
+    localStorage.setItem(/* nazwa obiektu */ JSON.stringify(/* nazwa obiektu */))
+}
+
