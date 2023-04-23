bd1dd3781b1db00948b3fbe08a4517803e47ccbd
EmiliaSzczepaniak52 <emiliaszczepaniak52@gmail.com>
2023-04-23 02:01:44 +0200

    spinner

--- src/JS/spinner.js
+++ src/JS/spinner.js
@@ -9,16 +9,18 @@ function hideLoader() {
     let target = document.getElementById('loader');
     target.removeChild(target.firstChild);
   }
-
-  axios.get('/api/data')
+  const API_KEY = '28f50cf3f177782503c21b43af04c7bc';
+  const SEARCH_API = `https://api.themoviedb.org/3/search/movie?&api_key=${API_KEY}&page=1&query=`;
+  axios.get(SEARCH_API)
   .then(function (response) {
-    // handle success
+    
   })
   .catch(function (error) {
-    // handle error
+     console.log("ERROR" + error)
   })
   .then(function () {
-    // always executed
+    
+    
     hideLoader();
   });
 
