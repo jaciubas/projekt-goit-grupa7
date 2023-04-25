3195fa9722c5ed80de85cc71edc1004df217f65f
Gniewomir368 <61843466+kuba368@users.noreply.github.com>
2023-04-25 16:10:54 +0200

    Update 1.13
    Working on movie search pagination

--- src/JS/movie-search.js
+++ src/JS/movie-search.js
@@ -1,40 +1,47 @@
 import { Spinner } from 'spin.js';
 import { opts } from './spinner';
+// import Pagination from 'tui-pagination';
+import pagination from './pagination';
+
 const searchMoviesForm = document.querySelector('#form');
 const searchMovieInput = document.querySelector('#topSearch');
 const main = document.querySelector('main');
 const searchErrorMsg = document.querySelector('.error-msg');
+const paginationTemplate = document.querySelector('#pagination');
 
+let page = 1;
 const API_KEY = '28f50cf3f177782503c21b43af04c7bc';
-const SEARCH_API = `https://api.themoviedb.org/3/search/movie?&api_key=${API_KEY}&page=1&query=`;
+const SEARCH_API = `https://api.themoviedb.org/3/search/movie?&api_key=${API_KEY}&page=${page}&query=`;
+
 const spinner = new Spinner(opts).spin();
 const loader = document.getElementById('loader');
+
 searchErrorMsg.classList.add('is-hidden');
 
 const getResults = async url => {
   try {
     const response = await fetch(url);
     const data = await response.json();
-    console.log(data);
     showResults(data.results);
     getGenresData(data.genres);
     if (data.results.length === 0) {
       searchErrorMsg.classList.remove('is-hidden');
       main.innerHTML = '';
+      paginationTemplate.classList.add('is-hidden');
       return;
     }
   } catch (error) {
     console.log(error);
   }
+  pagination.reset();
 };
 
 const searchPoster = movie => {
   let posterPath = movie.poster_path ? movie.poster_path : movie.backdrop_path;
   if (posterPath === null) {
-    return (IMAGE_PATH =
-      'https://images.pexels.com/photos/5721902/pexels-photo-5721902.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2');
+    return 'https://images.pexels.com/photos/5721902/pexels-photo-5721902.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2';
   } else {
-    return (IMAGE_PATH = `https://image.tmdb.org/t/p/original${posterPath}`);
+    return `https://image.tmdb.org/t/p/original${posterPath}`;
   }
 };
 
