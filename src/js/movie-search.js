3d22a96744b4881728e73435a502e812c0cd8e8c
jaciubas <114225522+jaciubas@users.noreply.github.com>
2023-04-27 18:27:41 +0200

    Add files via upload

---
+++ src/js/movie-search.js
@@ -0,0 +1,108 @@
+import { Spinner } from 'spin.js';
+import { opts } from './spinner';
+import pagination from './pagination';
+
+const searchMoviesForm = document.querySelector('#form');
+const searchMovieInput = document.querySelector('#topSearch');
+const main = document.querySelector('main');
+const searchErrorMsg = document.querySelector('.error-msg');
+const paginationTemplate = document.querySelector('#pagination');
+
+let page = 1;
+const API_KEY = '28f50cf3f177782503c21b43af04c7bc';
+const SEARCH_API = `https://api.themoviedb.org/3/search/movie?&api_key=${API_KEY}&page=${page}&query=`;
+
+const spinner = new Spinner(opts).spin();
+const loader = document.getElementById('loader');
+
+searchErrorMsg.classList.add('is-hidden');
+
+const getResults = async url => {
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    showResults(data.results);
+    getGenresData(data.genres);
+    pagination.reset();
+
+    if (data.results.length === 0) {
+      searchErrorMsg.classList.remove('is-hidden');
+      main.innerHTML = '';
+      paginationTemplate.classList.add('is-hidden');
+      return;
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const searchPoster = movie => {
+  let posterPath = movie.poster_path ? movie.poster_path : movie.backdrop_path;
+  if (posterPath === null) {
+    return 'https://images.pexels.com/photos/5721902/pexels-photo-5721902.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2';
+  } else {
+    return `https://image.tmdb.org/t/p/original${posterPath}`;
+  }
+};
+
+const showResults = movies => {
+  main.innerHTML = '';
+  movies.forEach(async movie => {
+    let { id, title, genres, year } = movie;
+    let IMAGE_PATH = searchPoster(movie);
+    const movieDate = movie.release_date ? movie.release_date : movie.first_air_date;
+    year = new Date(movieDate).getFullYear();
+    genres = await getGenresData(movie.genre_ids);
+    id = movie.id;
+
+    const movieElement = document.createElement('div');
+
+    movieElement.innerHTML = `<li class="movie__template">
+    <img class="movie__image" id="${id}" src="${IMAGE_PATH}" alt='${title}' loading="lazy" width="280px" height="398px"/> 
+    <h5 class="movie__title">${title}</h5>
+    <div class="movie__informations"><span>${genres}</span> | <span>${year}</span></div>
+  </li>`;
+    main.appendChild(movieElement);
+  });
+};
+
+const searchIdForName = (data, arrayOfIds) => {
+  const names = data
+    .filter(d => arrayOfIds.includes(d.id))
+    .map(d => {
+      return d.name;
+    });
+  return names;
+};
+
+const getGenresData = async arrayOfIds => {
+  loader.appendChild(spinner.el);
+  try {
+    const GENRES_PATH = `https://api.themoviedb.org/3/genre/movie/list?api_key=${API_KEY}`;
+    const response = await fetch(GENRES_PATH);
+    const data = await response.json();
+    const genresData = await data.genres;
+    if (arrayOfIds) {
+      const names = searchIdForName(genresData, arrayOfIds);
+      return names;
+    }
+  } catch (error) {
+    console.log(error);
+  } finally {
+    spinner.stop();
+  }
+};
+
+searchMoviesForm.addEventListener('submit', e => {
+  e.preventDefault();
+  window.scrollTo({ top: 0, behavior: 'smooth' });
+  const query = searchMovieInput.value;
+  if (query) {
+    getResults(SEARCH_API + query);
+    searchMovieInput.value = '';
+  }
+});
+
+pagination.on('afterMove', e => {
+  getResults(e.page);
+});
---
+++ src/js/pagination.js
@@ -0,0 +1,39 @@
+import { getPopularMoviesData } from './movie-template';
+import Pagination from 'tui-pagination';
+
+const options = {
+  totalItems: 1000,
+  itemsPerPage: 20,
+  visiblePages: 5,
+  page: 1,
+  centerAlign: true,
+  template: {
+    page: '<a href="#" class="tui-page-btn">{{page}}</a>',
+    currentPage: '<strong class="tui-page-btn tui-is-selected">{{page}}</strong>',
+    moveButton:
+      '<a href="#" class="tui-page-btn tui-{{type}}">' +
+      '<span class="tui-ico-{{type}}">{{type}}</span>' +
+      '</a>',
+    disabledMoveButton:
+      '<span class="tui-page-btn tui-is-disabled tui-{{type}}">' +
+      '<span class="tui-ico-{{type}}">{{type}}</span>' +
+      '</span>',
+    moreButton:
+      '<a href="#" class="tui-page-btn tui-{{type}}-is-ellip">' + '<span>⋅⋅⋅</span>' + '</a>',
+  },
+};
+
+const pagination = new Pagination('pagination', options);
+
+pagination.on('afterMove', function (eventData) {
+  window.scrollTo({ top: 0, behavior: 'smooth' });
+  const currentPage = eventData.page;
+  const startIndex = (currentPage - 1) * options.itemsPerPage;
+  const endIndex = startIndex + options.itemsPerPage;
+
+  if (currentPage > 1) {
+    getPopularMoviesData(startIndex, endIndex);
+  }
+});
+
+export default pagination;
